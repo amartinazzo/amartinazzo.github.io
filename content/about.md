905128faa576b075b4e46621af316c80994cc6e5
amartina@ime.usp.br <amartina@ime.usp.br>
2019-04-30 23:46:25 -0300

    add hugo

---
+++ content/about.md
@@ -0,0 +1,7 @@
++++
+title = "about"
+date = "2019"
+aliases = ["about", "contact"]
++++
+
+I am a mechanical engineer turned computer scientist.
---
+++ publish.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#DIR=$(dirname "$0")
+
+#cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "the working directory is dirty. please commit any pending changes."
+    exit 1;
+fi
+
+echo "deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "removing public folder"
+rm -rf public/*
+
+echo "generating site"
+HUGO_ENV="production" hugo
+
+echo "updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "pushing to publish"
+git push origin gh-pages -f

d7f3854d74b39037de6f29d1f73b9dab95572b50
amartina@ime.usp.br <amartina@ime.usp.br>
2019-05-02 20:40:19 -0300

    add console debug mode in js

--- static/browserponies.js
+++ static/browserponies.js
@@ -22,6 +22,15 @@
 
 "use strict";
 
+var DEBUG = false;
+if(!DEBUG){
+    if(!window.console) window.console = {};
+    var methods = ["log", "debug", "warn", "info"];
+    for(var i=0;i<methods.length;i++){
+        console[methods[i]] = function(){};
+    }
+}
+
 if (typeof(BrowserPonies) !== "object") {
 
 // Shims:
